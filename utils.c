501be0fddbda8143153e24acef637bea2d89045b
nautrw <nautrw@proton.me>
2026-05-26 16:29:19 -0400

    format code

--- utils.c
+++ utils.c
@@ -1,24 +1,20 @@
+#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
-int get_index(const char **arr, size_t size, char *target)
-{
-	for (int i = 0; i < size; i++)
-	{
-		if (strcmp(arr[i], target) == 0)
-			return i;
-	}
+int get_index(const char **arr, size_t size, char *target) {
+  for (int i = 0; i < size; i++) {
+    if (strcmp(arr[i], target) == 0)
+      return i;
+  }
 
-	return -1;
+  return -1;
 }
 
-char *strlower(char *str)
-{
-	for (int i = 0; str[i]; i++)
-	{
-		str[i] = tolower(str[i]);
-	}
+char *strlower(char *str) {
+  for (int i = 0; str[i]; i++) {
+    str[i] = tolower(str[i]);
+  }
 
-	return str;
+  return str;
 }
